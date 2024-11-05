397bd7f6563535eee20826c2f310707fe611fa79
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-11-05 08:23:56 +0200

    add variables and functions to carry out operations based on the operator variable

--- script.js
+++ script.js
@@ -10,8 +10,61 @@ const buttons = calcSymbols.map((symbol) => {
     return button;
 })
 
+// variables
+
+let num1;
+let operator;
+let num2;
+let result;
+
+// helper
+
+function getNum1() {
+    return num1;
+}
+
+function setNum1(num) {
+    num1 = num;
+}
+
+function getNum2() {
+    return num2;
+}
+
+function setNum2(num) {
+    num2 = num;
+}
+
+function getOperator() {
+    return operator;
+}
+
+function setOperator(sign) {
+    operator = sign;
+}
 
 // calculator functions
+
+// operate
+function operate(operator, num1, num2) {
+
+    switch (operator) {
+        case '+':
+            result = add(num1,num2);
+            break;
+        case '-':
+            result = subtract(num1,num2);
+            break;
+        case '*':
+            result = multiply(num1,num2);
+            break;
+        case '/':
+            result = divide(num1,num2);
+            break;
+    }
+
+
+}
 
 // add
 function add(a,b) {
